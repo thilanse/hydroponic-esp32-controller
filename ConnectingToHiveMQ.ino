5d90aa14501948434cc9bf51b6ebfe76a66ed119
Thilan Senanayake <thilansenanayake@gmail.com>
2022-11-14 23:52:24 +0530

    added program

---
+++ ConnectingToHiveMQ.ino
@@ -0,0 +1,197 @@
+/*
+ * Virtuino MQTT getting started example
+ * Broker: HiveMQ (Secure connection)
+ * Supported boards: ESP8266 / ESP32 
+ * Created by Ilias Lamprou
+ * Jul 13 2021
+ *
+ * Modified by Thilan Senanayake
+ * Nov 14 2022
+ */
+
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+
+//---- WiFi settings
+const char* ssid = "Dialog 4G 500";
+const char* password = "8E545Ce1";
+
+//---- MQTT Broker settings
+const char* mqtt_server = "f9a63d04e24b4540ab8742be4a41cce7.s2.eu.hivemq.cloud";  // replace with your broker url
+const char* mqtt_client = "ESP32-1";
+const char* mqtt_username = "thilan-hive-mq";
+const char* mqtt_password = "ThiHive.2853";
+const int mqtt_port = 8883;
+
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+
+
+// Initialize sensor values
+int sensor1 = 0;
+float sensor2 = 0;
+int command1 = 0;
+
+// Sensor publish topics
+const char* sensor1_topic = "sensor1";
+const char* sensor2_topic = "sensor2";
+//const char*  sensor2_topic="sensor3";
+
+// Command topics
+const char* command1_topic = "command1";
+//const char* command1_topic="command2";
+
+int PUBLISH_INTERVAL = 5000;
+
+
+
+
+
+static const char* root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+
+//==========================================
+void setup_wifi() {
+  delay(10);
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("\nWiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+//=====================================
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    // Attempt to connect
+    if (client.connect(mqtt_client, mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+
+      client.subscribe(command1_topic);  // subscribe the topics here
+      //client.subscribe(command2_topic);   // subscribe the topics here
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");  // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//================================================ setup
+//================================================
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) delay(1);
+  setup_wifi();
+  pinMode(2, OUTPUT);  // Initialize the BUILTIN_LED pin as an output
+
+  espClient.setCACert(root_ca);
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void publishData() {
+  // Collect sensor data and publish to relevant topics
+  
+  sensor1 = random(50);       // replace the random value with your sensor value
+  sensor2 = 20 + random(80);  // replace the random value  with your sensor value
+  publishMessage(sensor1_topic, String(sensor1), true);
+  publishMessage(sensor2_topic, String(sensor2), true);
+}
+
+
+//================================================ loop
+//================================================
+void loop() {
+
+  if (!client.connected()) reconnect();
+  client.loop();
+
+  // Publish sensor data after every time interval configured by PUBLISH_INTERVAL
+  unsigned long now = millis();
+  if (now - lastMsg > PUBLISH_INTERVAL) {
+    lastMsg = now;
+    publishData();
+  }
+}
+
+//=======================================
+// This void is called every time we have a message from the broker
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage += (char)payload[i];
+
+  Serial.println("Message arrived [" + String(topic) + "]" + incommingMessage);
+
+  //--- check the incomming message
+  if (strcmp(topic, command1_topic) == 0) {
+    if (incommingMessage.equals("1")) digitalWrite(2, HIGH);  // Turn the LED on
+    else digitalWrite(2, LOW);                                // Turn the LED off
+  }
+
+  //  check for other commands
+  /*  else  if( strcmp(topic,command2_topic) == 0){
+     if (incommingMessage.equals("1")) {  } // do something else
+  }
+  */
+}
+
+
+
+//======================================= publising as string
+void publishMessage(const char* topic, String payload, boolean retained) {
+  if (client.publish(topic, payload.c_str(), true))
+    Serial.println("Message publised [" + String(topic) + "]: " + payload);
+}
